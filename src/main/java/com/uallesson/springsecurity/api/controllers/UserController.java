136e383f7ca5b1352ccb3e2063ebf8445b8a67f3
Uallessonivo <uallessons@gmail.com>
2023-11-28 13:11:23 -0300

    Implement UserController

---
+++ src/main/java/com/uallesson/springsecurity/api/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.uallesson.springsecurity.api.controllers;
+
+import com.uallesson.springsecurity.domain.entities.User;
+import com.uallesson.springsecurity.domain.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        return userService.findByUsername(username);
+    }
+
+    @GetMapping("/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.findByUsername(email);
+    }
+
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PostMapping("/roles")
+    public User saveRole(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PostMapping("/users/{username}/roles/{roleName}")
+    public void addRoleToUser(@PathVariable String username, @PathVariable String roleName) {
+        userService.addRoleToUser(username, roleName);
+    }
+}
